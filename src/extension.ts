ec33fafa989d72bc220edffb839012b73edd8c7d
Artem Prokop <artem13.prokop@gmail.com>
2025-06-09 01:05:13 +0200

    feat: rate-limit, per-app colors

--- src/extension.ts
+++ src/extension.ts
@@ -1,11 +1,89 @@
-import { Extension } from 'resource:///org/gnome/shell/extensions/extension.js';
+import { Extension } from "resource:///org/gnome/shell/extensions/extension.js";
+import * as Main from "resource:///org/gnome/shell/ui/main.js";
+import * as notificationDaemonModule from "resource:///org/gnome/shell/ui/notificationDaemon.js";
+
+// TODO: Take from settings
+const NOTIFICATION_THRESHOLD = 5_000;
+
+// TODO: Proper types
 
 export default class GnomeShellExtension extends Extension {
-	enable() {
-		console.log('GnomeShellExtension | enabled');
-	}
+  private _processNotification!: () => void;
+  private themeSignalId: number | null = null;
 
-	disable() {
-		console.log('GnomeShellExtension | disabled');
-	}
+  private get notificationDaemonSourcePrototype() {
+    return (notificationDaemonModule as any).FdoNotificationDaemonSource
+      .prototype;
+  }
+
+  private timings: Record<string, number> = {};
+  // TODO: Take from settings
+  private colors: Record<string, string | undefined> = {
+    telegram: "#0e1621",
+  };
+
+  enable() {
+    this.enableRateLimit();
+    this.enableThemes();
+  }
+
+  disable() {
+    this.notificationDaemonSourcePrototype.processNotification =
+      this._processNotification;
+
+    if (this.themeSignalId !== null) {
+      const messageTrayContainer = Main.messageTray.get_first_child()!;
+      messageTrayContainer.disconnect(this.themeSignalId);
+    }
+  }
+
+  private enableRateLimit() {
+    const originalProcessNotification = (this._processNotification =
+      this.notificationDaemonSourcePrototype.processNotification);
+
+    const { timings } = this;
+
+    this.notificationDaemonSourcePrototype.processNotification = function (
+      notification: any,
+      ...rest: any
+    ) {
+      const lastNotification = timings[notification.title];
+      if (
+        lastNotification &&
+        Date.now() - lastNotification < NOTIFICATION_THRESHOLD
+      ) {
+        console.log(
+          `Notification from ${notification.title} was sent too fast - hiding (${Date.now() - lastNotification}s / ${NOTIFICATION_THRESHOLD}s)`,
+        );
+        notification.acknowledged = true;
+      } else {
+        timings[notification.title] = Date.now();
+        console.log(`Notification from ${notification.title} - showing`);
+      }
+      originalProcessNotification.call(this, notification, ...rest);
+    };
+  }
+
+  private enableThemes() {
+    const messageTrayContainer = Main.messageTray.get_first_child()!;
+    this.themeSignalId = messageTrayContainer.connect("child-added", () => {
+      const notificationContainer =
+        messageTrayContainer?.get_first_child() as any;
+
+      const notification = notificationContainer?.get_first_child() as any;
+
+      const appName = (
+        notification // Content container
+          ?.get_first_child() // Header
+          ?.get_child_at_index(1) // Text container
+          ?.get_first_child() as any
+      )?.get_text() as string;
+
+      const color = this.colors[appName];
+
+      if (color) {
+        notificationContainer?.set_style(`background:${color};`);
+      }
+    });
+  }
 }
